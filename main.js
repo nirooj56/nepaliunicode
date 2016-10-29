e7478d7238c6ec32cbd839a558208b552d73eae8
Bista Nirooj <nirooj56@gmail.com>
2016-10-29 12:49:35 +0545

    main.js file created

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// Quit when all windows are closed. 
 app.on('window-all-closed', () => {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
